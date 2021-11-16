20fc4a63b93c57be5aaab44d9a025dd638536937
Celdama <celdama.dev@gmail.com>
2021-11-16 16:21:10 +0100

    added btn to change gif without reload page

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const img = document.querySelector('img')
+const btn = document.querySelector('button')
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=jbAy49ozGN0Qs3mgXOJmbvFVf9eOvZRm&s=cats', {
-  mode: 'cors'
-})
-  .then((res) => {
-    return res.json()
+const getCatGif = () => {
+  fetch('https://api.giphy.com/v1/gifs/translate?api_key=jbAy49ozGN0Qs3mgXOJmbvFVf9eOvZRm&s=cats', {
+    mode: 'cors'
   })
-  .then(res => {
-    img.src = res.data.images.original.url
-})
+    .then((res) => {
+      return res.json()
+    })
+    .then(res => {
+      img.src = res.data.images.original.url
+  })
+}
+
+
+
+btn.addEventListener('click', () => getCatGif())
+
+window.onload(getCatGif())
