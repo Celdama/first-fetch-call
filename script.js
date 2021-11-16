9d4e1c608633d28258ef10f17d04d7703e587a38
Celdama <celdama.dev@gmail.com>
2021-11-16 16:38:44 +0100

    added search bar to allow users search for specific gifs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const img = document.querySelector('img')
 const btn = document.querySelector('button')
+const form = document.querySelector('form')
 
 const getCatGif = () => {
   fetch('https://api.giphy.com/v1/gifs/translate?api_key=jbAy49ozGN0Qs3mgXOJmbvFVf9eOvZRm&s=cats', {
@@ -19,5 +20,38 @@ const getCatGif = () => {
 
 
 btn.addEventListener('click', () => getCatGif())
+
+
+const getSearchBarValue = () => {
+  const input = document.querySelector('input')
+  return input.value
+}
+
+const getGiff = (userChoice) => {
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=jbAy49ozGN0Qs3mgXOJmbvFVf9eOvZRm&s=${userChoice}`, {
+    mode: 'cors'
+  })
+    .then((res) => {
+      return res.json()
+    })
+    .then(res => {
+      img.src = res.data.images.original.url
+    })
+    .catch(error => {
+      alert('Your keyword dont match any gif, please try again')
+      console.log(error);
+  })
+}
+
+
+form.addEventListener('submit', () => {
+  const query = getSearchBarValue()
+  getGiff(query)
+  form.reset()
+})
+
+
+
+
 
 window.onload = getCatGif()
