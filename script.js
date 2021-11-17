1efaffa8efb011c8a94dd44e73b0732310007b2a
Celdama <celdama.dev@gmail.com>
2021-11-17 07:39:55 +0100

    add practice function

--- script.js
+++ script.js
@@ -46,3 +46,41 @@ btn.addEventListener('click', () => getGiff())
 
 
 window.onload = getGiffWithAsync()
+
+
+
+
+
+// EN DESSOUS RIEN A VOIR AVEC L'APP
+// C'EST JUSTE UN CAS PRATIQUE DE REECRITURE DE FONCTION EN UTILISANT ASYNC AWAIT
+
+// const doubleAfter2Seconds = (x) => {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve(x * 2)
+//     }, 2000);
+//   })
+// }
+
+// const addPromise = (x) => {
+//   return new Promise(resolve => {
+//     doubleAfter2Seconds(10).then((a) => {
+//       doubleAfter2Seconds(20).then((b) => {
+//         doubleAfter2Seconds(30).then((c) => {
+//           resolve(x + a + b + c);
+//       	})
+//       })
+//     })
+//   });
+// }
+
+// const addAsync = async (x) => {
+//   const a = await doubleAfter2Seconds(10)
+//   const b = await doubleAfter2Seconds(20)
+//   const c = await doubleAfter2Seconds(30)
+//   return x + a + b + c
+// }
+
+// addAsync(10).then(sum => {
+//   console.log(sum)
+// })
